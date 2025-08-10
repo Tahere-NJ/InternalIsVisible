17ba3bed40667160f59a02fa1275eb72412d2866
Tahere-NJ <alivedead65@gmail.com>
2025-08-10 15:44:29 +0330

    Add project files.

---
+++ InternalIsVisible.Core/InternalClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalIsVisible.Core
+{
+    internal class InternalCalculator
+    {
+        internal int Add(int a, int b) => a + b;
+
+        internal static string SecretMessage => "This is normally hidden!";
+    }
+}
---
+++ InternalIsVisible.Tests/InternalClassTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternalIsVisible.Core;
+
+namespace InternalIsVisible.Tests
+{
+    public class InternalClassTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            // Arrange
+            var calculator = new InternalCalculator();
+
+            // Act
+            var result = calculator.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void SecretMessage_ReturnsExpectedValue()
+        {
+            // Act
+            var message = InternalCalculator.SecretMessage;
+
+            // Assert
+            Assert.Equal("This is normally hidden!", message);
+        }
+    }
+}
